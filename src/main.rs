fc2d3f0949f5db840843c5a61e097f1ec52e0a36
PP6464 <panthhp@outlook.com>
2026-09-24 22:19:56 +0100

    test everything works together

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use trading_bot::client::client::Client;
+use trading_bot::config::config::Config;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config =
+        Config::load().expect("Config could not loaded - cannot proceed. Shutting down.");
+    let client = Client::from_config(&config)
+        .expect("HTTP client could not be initialised - cannot proceed. Shutting down.");
+
+    let resp = client.get("equity/account/info").send().await?;
+
+    println!("Status: {}", resp.status());
+    println!("Headers: {:?}", resp.headers());
+    println!("{}", resp.text().await?);
+    Ok(())
 }
