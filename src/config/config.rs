1dc0d943264ca061d0bcde62576dbf98b016097a
PP6464 <panthhp@outlook.com>
2026-09-24 22:31:44 +0100

    restructure how config is loaded

--- src/config/config.rs
+++ src/config/config.rs
@@ -10,15 +10,19 @@ impl Config {
     /// This ensures the API url ends in a trailing backslash.
     pub fn load() -> Option<Config> {
         dotenvy::dotenv().ok()?;
-        let env = std::env::var("ENV").ok()?;
+        Self::from_env(|key| std::env::var(key).ok())
+    }
+
+    pub fn from_env(get_var: impl Fn(&str) -> Option<String>) -> Option<Config> {
+        let env = get_var("ENV")?;
         match env.as_str() {
             "demo" => {
-                let api_url = std::env::var("DEMO_API_URL").ok()?;
+                let api_url = get_var("DEMO_API_URL")?;
                 if !api_url.ends_with('/') {
                     return None;
                 }
-                let api_key = std::env::var("DEMO_API_KEY").ok()?;
-                let api_secret = std::env::var("DEMO_API_SECRET").ok()?;
+                let api_key = get_var("DEMO_API_KEY")?;
+                let api_secret = get_var("DEMO_API_SECRET")?;
                 Some(Config {
                     api_url_base: api_url,
                     api_key,
@@ -26,12 +30,12 @@ impl Config {
                 })
             }
             "live" => {
-                let api_url = std::env::var("LIVE_API_URL").ok()?;
+                let api_url = get_var("LIVE_API_URL")?;
                 if !api_url.ends_with('/') {
                     return None;
                 }
-                let api_key = std::env::var("LIVE_API_KEY").ok()?;
-                let api_secret = std::env::var("LIVE_API_SECRET").ok()?;
+                let api_key = get_var("LIVE_API_KEY")?;
+                let api_secret = get_var("LIVE_API_SECRET")?;
                 Some(Config {
                     api_url_base: api_url,
                     api_key,
